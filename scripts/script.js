c7ba918892f51b1c8393f6d04ef38127ca35ee01
Hexterty46 <efsuslusemih@gmail.com>
2025-09-10 11:32:47 +0200

    De search functie

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const SearchInput = document.querySelector('.search input[type="search"]');
+SearchInput.addEventListener('input', function() {
+    const filter = SearchInput.value.toLowerCase();
+    const mugshots = document.querySelectorAll('.mugshot');
+
+    mugshots.forEach(mugshot => {
+        const name = mugshot.querySelector('h3').textContent.toLowerCase()
+
+        if(name.includes(filter)) {
+            mugshot.style.display = '';
+        } else {
+            mugshot.style.display = 'none'
+        }
+    })
+});
